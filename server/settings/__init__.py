1e0527b6dc5e0b0b925b6e6831dddae62f04861d
kyomi <me@kyomi.dev>
2023-09-26 10:16:50 -0300

    feat(templates): add the initial project templates

--- server/settings/__init__.py
+++ server/settings/__init__.py
@@ -1,13 +1,14 @@
-from os.path import join
 from pathlib import Path
 
 from environ import Env
 
 BASE_DIR = Path(__file__).parent.parent.parent
-APPS_DIR = BASE_DIR / "apps"
 
 # Load environment variables from config/.env file. See
 # https://django-environ.readthedocs.io/en/latest/
 
 env = Env()
-env.read_env(join(BASE_DIR, "config", ".env"))
+READ_DOTENV = env.bool("DJANGO_READ_DOTENV", default=False)
+
+if READ_DOTENV:
+    env.read_env(BASE_DIR / "config" / ".env")
--- server/urls.py
+++ server/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.views.generic import TemplateView
+
+home_view = TemplateView.as_view(template_name="pages/home.html")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("django.contrib.auth.urls")),
+    path("", home_view, name="home"),
 ]
