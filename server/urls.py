8c8a6808baeeb5fb0c1d0f3102a9b5f0e9efa5d6
LuizaMaluf <luizamaluf03@gmail.com>
2023-09-28 16:07:28 -0300

    feat(templates): add initial registering template

--- server/urls.py
+++ server/urls.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
 
+
 home_view = TemplateView.as_view(template_name="pages/home.html")
+register_view = TemplateView.as_view(template_name="registration/register.html")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("django.contrib.auth.urls")),
-    path("", home_view, name="home"),
+    path("", home_view, name="home"), 
+    path("register/", register_view, name="register")  
 ]
