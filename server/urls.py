dc43a32ba5413fb073b219d8c809d1b7c235b7c0
kyomi <me@kyomi.dev>
2023-09-28 20:40:54 -0300

    style: fix code code style

--- server/urls.py
+++ server/urls.py
@@ -2,13 +2,16 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
 
-
 home_view = TemplateView.as_view(template_name="pages/home.html")
-register_view = TemplateView.as_view(template_name="registration/register.html")
+register_view = TemplateView.as_view(
+    template_name="registration/register.html"
+)
 
 urlpatterns = [
+    # Django views
     path("admin/", admin.site.urls),
     path("", include("django.contrib.auth.urls")),
-    path("", home_view, name="home"), 
-    path("register/", register_view, name="register")  
+    # Local views
+    path("", home_view, name="home"),
+    path("register/", register_view, name="register"),
 ]
