dd58307316b14a036fbe923d6255dc3e2d3d0ea4
kyomi <me@kyomi.dev>
2023-09-30 11:23:50 -0300

    feat(apps/contests): save the sent code as a string buffer

--- apps/contests/views.py
+++ apps/contests/views.py
@@ -1,3 +1,7 @@
+import sys
+from io import StringIO
+from typing import TYPE_CHECKING
+
 from django.db.models.query import QuerySet
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import get_object_or_404
@@ -5,8 +9,15 @@ from django.views import generic
 
 from apps.contests.models import Contest
 
+if TYPE_CHECKING:
+    IndexViewBase = generic.ListView[Contest]
+    DetailViewBase = generic.DetailView[Contest]
+else:
+    IndexViewBase = generic.ListView
+    DetailViewBase = generic.DetailView
 
-class IndexView(generic.ListView[Contest]):
+
+class IndexView(IndexViewBase):
     template_name = "contests/index.html"
     context_object_name = "contests"
 
@@ -14,7 +25,7 @@ class IndexView(generic.ListView[Contest]):
         return Contest._default_manager.order_by("-start_time")[:5]
 
 
-class DetailView(generic.DetailView[Contest]):
+class DetailView(DetailViewBase):
     model = Contest
     template_name = "contests/detail.html"
 
@@ -23,6 +34,18 @@ def send(request: HttpRequest, contest_id: int) -> HttpResponse:
     contest = get_object_or_404(Contest, pk=contest_id)
     code = request.POST["code"]
 
-    eval(code)
+    old_stdout = sys.stdout
+    sys.stdout = buffer = StringIO()
 
-    return HttpResponse(f"Contest {contest.title} ran successfully.")
+    try:
+        eval(code)
+    except Exception as exc:
+        sys.stdout = old_stdout
+        return HttpResponse(f"Contest {contest.title} failed.\n{exc}")
+
+    sys.stdout = old_stdout
+    message = buffer.getvalue()
+
+    return HttpResponse(
+        f"Contest {contest.title} ran successfully.\n{message}"
+    )
