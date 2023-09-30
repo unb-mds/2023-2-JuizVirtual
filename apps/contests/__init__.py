6ff047c52bc371444062e0e53a7d2727b10f3e53
kyomi <me@kyomi.dev>
2023-09-30 02:12:14 -0300

    feat(apps/contests): add the initial contests app

---
+++ apps/contests/__init__.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+default_app_config = "apps.contests.ContestsConfig"
+
+
+class ContestsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "appscontests"
