b8fb81b3bbbab721caf8a983153d23ab58594a24
kyomi <me@kyomi.dev>
2023-09-30 00:29:35 -0300

    feat(apps/users): add the initial users app

---
+++ apps/users/admin.py
@@ -0,0 +1,34 @@
+from django.contrib.admin import register
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
+from django.utils.translation import gettext_lazy as _
+
+from apps.users.models import User
+
+
+@register(User)
+class UserAdmin(DefaultUserAdmin):
+    list_display = ("username", "email", "is_staff", "is_active")
+    fieldsets = [
+        (_("Personal info"), {"fields": ("username", "email", "password")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "user_permissions",
+                    "groups",
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                )
+            }
+        ),
+    ]
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "email", "password1", "password2"),
+            },
+        ),
+    )
