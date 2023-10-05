6b27a6cc6a3c1f30ec9a76a3c7fac6f857b2cb0c
kyomi <me@kyomi.dev>
2023-10-05 00:12:40 -0300

    feat(templates/contests): improve the contest details page

--- apps/problems/admin.py
+++ apps/problems/admin.py
@@ -1,7 +1,7 @@
 from typing import TYPE_CHECKING
 
 from django.contrib.admin import ModelAdmin, register
-from django.forms import CharField, ModelForm, Textarea
+from django.forms import CharField, IntegerField, ModelForm, Textarea
 from django.utils.translation import gettext_lazy as _
 
 from apps.problems.models import Problem
@@ -16,6 +16,14 @@ else:
 
 class ProblemModelForm(ProblemModelFormBase):
     description = CharField(widget=Textarea(attrs={"rows": 14, "cols": 80}))
+    score = IntegerField(min_value=0, required=False)
+
+    memory_limit = IntegerField(
+        min_value=0, required=False, help_text=_("In bytes.")
+    )
+    time_limit = IntegerField(
+        min_value=0, required=False, help_text=_("In seconds.")
+    )
 
     class Meta:
         model = Problem
@@ -26,6 +34,11 @@ class ProblemModelForm(ProblemModelFormBase):
 class ProblemAdmin(ProblemAdminBase):
     form = ProblemModelForm
 
+    list_display = ("title", "contest", "memory_limit", "time_limit")
+    list_filter = ("contest", "score")
+
     fieldsets = [
-        (_("Problem"), {"fields": ("title", "description", "contest")}),
+        (_("General"), {"fields": ("title", "description")}),
+        (_("Meta"), {"fields": ("contest", "score")}),
+        (_("Limits"), {"fields": ("memory_limit", "time_limit")}),
     ]
--- apps/problems/models.py
+++ apps/problems/models.py
@@ -1,5 +1,6 @@
-from django.db.models import CASCADE, CharField, ForeignKey
+from django.db.models import CASCADE, CharField, ForeignKey, IntegerField
 
+from apps.contests.enums import ContestStatus
 from apps.contests.models import Contest
 from core.models import TimestampedModel
 
@@ -11,9 +12,20 @@ class Problem(TimestampedModel):
     description = CharField(max_length=4096)
 
     contest = ForeignKey(Contest, related_name="problems", on_delete=CASCADE)
+    score = IntegerField(null=True)
+
+    memory_limit = IntegerField(null=True)
+    time_limit = IntegerField(null=True)
 
     class Meta:
         db_table = "problems"
 
     def __str__(self) -> str:
         return self.title
+
+    @property
+    def is_accessible(self) -> bool:
+        return self.contest.status in (
+            ContestStatus.RUNNING,
+            ContestStatus.FINISHED,
+        )
