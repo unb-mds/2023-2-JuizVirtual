073bf8240bf5bd7d8f53434de7d2457c8d291fec
LuizaMaluf <luizamaluf03@gmail.com>
2023-10-31 22:35:29 -0300

    test(auth): User test case

--- apps/users/tests_users.py
+++ apps/users/tests_users.py
@@ -1,8 +1,22 @@
 from django.test import TestCase
 
+from .models import User
 
-class UserTestCase(TestCase):
-    pass
+
+class UserModelTestCase(TestCase):
+    def test_user_fields(self) -> None:
+        self.user_data = {
+            "email": "test@example.com",
+            "username": "user_test",
+            "password": "passwordtest",
+        }
+        self.user = User.objects.create(**self.user_data)
+
+    def test_username_field_is_email(self) -> None:
+        self.assertEqual(User().USERNAME_FIELD, "email")
+
+    def test_required_fields_include_username(self) -> None:
+        self.assertIn("username", User().REQUIRED_FIELDS)
 
 
 class UserManagerTastCase(TestCase):
