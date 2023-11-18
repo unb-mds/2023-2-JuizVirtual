3bdde58cf028606cea4685d89071ff7b816c9008
kyomi <me@kyomi.dev>
2023-11-17 22:09:16 -0300

    docs(apps/users): document the login authentication backend

--- apps/users/views.py
+++ apps/users/views.py
@@ -18,12 +18,10 @@ class RegisterView(View):
         if form.is_valid():
             user = form.save()
 
-            login(
-                request,
-                user,
-                backend="django.contrib.auth.backends.ModelBackend",
-            )
-
+            # Since we have multiple authentication backends, we need to
+            # specify which one we want to use. In this case, we want to
+            # use the :class:`ModelBackend`, which is the default one.
+            login(request, user, "django.contrib.auth.backends.ModelBackend")
             return redirect("home")
 
         return render(request, self.template_name, {"form": form})
