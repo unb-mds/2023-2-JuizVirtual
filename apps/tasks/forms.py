e51d7e14bdfb31fba1c6af8703723b3df8b15c6b
thegm445 <gabrielmourajulho2005@gmail.com>
2023-11-20 19:11:55 -0300

    feat(submission-form): code source box is now bigger

--- apps/tasks/forms.py
+++ apps/tasks/forms.py
@@ -1,5 +1,9 @@
+from django import forms
 from django.forms import CharField, Form
 
 
 class TaskForm(Form):
-    code = CharField(label="Source Code")
+    code = CharField(
+        label="Source Code",
+        widget=forms.Textarea(attrs={"style": "height: 300px;"}),
+    )
