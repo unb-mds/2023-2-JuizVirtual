f0f8cc0f987f43b5e6cb12fb4143e2be04b64fe7
LuizaMaluf <luizamaluf03@gmail.com>
2023-11-23 21:02:39 -0300

    feat(submission_admin_page): admin page

---
+++ apps/submissions/admin.py
@@ -0,0 +1,40 @@
+from typing import TYPE_CHECKING
+
+from django.contrib.admin import ModelAdmin, register
+from django.forms import CharField, ModelForm, Textarea
+from django.utils.translation import gettext_lazy as _
+
+from apps.submissions.models import Submission
+
+if TYPE_CHECKING:
+    SubmissionAdminBase = ModelAdmin[Submission]
+    SubmissionModelFormBase = ModelForm[Submission]
+else:
+    SubmissionAdminBase = ModelAdmin
+    SubmissionModelFormBase = ModelForm
+
+
+class SubmissionModelForm(SubmissionModelFormBase):
+    submission_data = CharField(
+        widget=Textarea(attrs={"rows": 10, "cols": 80})
+    )
+
+    class Meta:
+        model = Submission
+        fields = "__all__"
+
+
+@register(Submission)
+class SubmissionAdmin(SubmissionAdminBase):
+    form = SubmissionModelForm
+
+    list_display = ("author", "task", "status", "created_at")
+    list_filter = ("status", "task")
+    search_fields = ("author__username", "task__title")
+
+    fieldsets = [
+        (
+            _("Submission Details"),
+            {"fields": ("author", "task", "code", "status", "created_at")},
+        )
+    ]
