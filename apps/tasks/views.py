4904e2527945e7c4792acc3d6863ca7c758e42a4
kyomi <me@kyomi.dev>
2023-11-26 20:15:46 -0300

    feat(apps/submissions): add tasks test cases feature

--- apps/tasks/views.py
+++ apps/tasks/views.py
@@ -1,3 +1,6 @@
+import sys
+from io import StringIO
+from traceback import format_exc
 from typing import TYPE_CHECKING, Any, Dict
 
 from django.http import HttpRequest, HttpResponse
@@ -16,6 +19,44 @@ if TYPE_CHECKING:
 else:
     DetailViewBase = generic.DetailView
     FormMixinBase = FormMixin
+
+
+def handle_submission(request: HttpRequest, task: Task) -> HttpResponse:
+    submission = Submission._default_manager.create(
+        code=request.POST["code"],
+        task=task,
+        author=request.user,
+    )
+
+    input_data = StringIO(task.input_file)
+
+    old_stdin = sys.stdin
+    sys.stdin = input_data
+
+    old_stdout = sys.stdout
+    sys.stdout = stdout = StringIO()
+
+    try:
+        eval(compile(request.POST["code"], "<string>", "exec"))
+    except Exception as exc:
+        submission.status = "RE"
+        submission.save()
+
+        return HttpResponse(f"Error: {exc} {format_exc()}")
+    finally:
+        sys.stdout = old_stdout
+        sys.stdin = old_stdin
+
+    output = stdout.getvalue()
+
+    if output == task.output_file:
+        submission.status = "AC"
+        submission.save()
+        return HttpResponse("Correct!")
+    else:
+        submission.status = "WA"
+        submission.save()
+        return HttpResponse("Incorrect!")
 
 
 class DetailView(FormMixinBase, DetailViewBase):
@@ -42,13 +83,7 @@ class DetailView(FormMixinBase, DetailViewBase):
         self.object = self.get_object()
         form = self.get_form()
 
-        if form.is_valid():
-            Submission._default_manager.create(
-                code=request.POST, task=self.object, author=request.user
-            )
+        if not form.is_valid():
+            return self.form_invalid(form)
 
-        return (
-            self.form_valid(form)
-            if form.is_valid()
-            else self.form_invalid(form)
-        )
+        return handle_submission(request, self.object)
