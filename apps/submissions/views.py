e2c2ebe7ff146bcb4a54149a9595e18f91443b77
LuizaMaluf <luizamaluf03@gmail.com>
2023-11-29 10:23:16 -0300

    feat(submission_page): submissions list tests

--- apps/submissions/views.py
+++ apps/submissions/views.py
@@ -1,9 +1,16 @@
+from typing import TYPE_CHECKING
+
 from django.views.generic import ListView
 
 from apps.submissions.models import Submission
 
+if TYPE_CHECKING:
+    SubmissionViewBase = ListView[Submission]
+else:
+    SubmissionViewBase = ListView
 
-class SubmissionListView(ListView[Submission]):
+
+class SubmissionListView(SubmissionViewBase):
     model = Submission
     template_name = "submission_list.html"
     context_object_name = "submissions"
